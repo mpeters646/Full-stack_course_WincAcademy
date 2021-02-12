15ad0d4afccf07f38c802ec144485259680b262c
mpeters646 <m.peters646@gmail.com>
2021-02-12 17:50:26 +0100

    Opdracht - For loops

---
+++ Level_14_Opdracht-6/grade-assigner.js
@@ -0,0 +1,18 @@
+function assignGrade(score) {
+  if (score > 90) {
+    return 'A';
+  } else if (score > 80) {
+    return 'B';
+  } else if (score > 70) {
+    return 'C';
+  } else if (score > 65) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+for (i = 60; i <= 100; i++) {
+  assignGrade((score = i));
+  console.log(`Voor ${i} punten, krijg je een ${assignGrade(score)}`);
+}
