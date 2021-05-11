1481aed07f9acc66611c28600a8464ca3ed84ffd
Marco Peters <m.peters646@gmail.com>
2021-05-11 19:27:05 +0200

    modified index.html & ouwecreditcards.js & added css/style.css

--- Level_24_Opdracht-1/js/ouwecreditcards.js
+++ Level_24_Opdracht-1/js/ouwecreditcards.js
@@ -2,19 +2,45 @@
 
 const person = () => {
   for (let i = 0; i < randomPersonData.length; i++) {
-    const fullName = `Fullname: ${randomPersonData[i].name} ${randomPersonData[i].surname}`;
-    const phoneNumber = `Phonenumber: ${randomPersonData[i].phone}`;
-    const ccNumber = `CreditCard Number: ${randomPersonData[i].credit_card.number}`;
-    const expirationDate = `Expiration Date: ${randomPersonData[i].credit_card.expiration}`;
+    const fullName = `${randomPersonData[i].name} ${randomPersonData[i].surname}`;
+    const phoneNumber = `Phone: ${randomPersonData[i].phone}`;
+    const ccNumber = `Card: ${randomPersonData[i].credit_card.number}`;
+    const expirationDate = `Expires: ${randomPersonData[i].credit_card.expiration}`;
     const person = `${fullName}\n${phoneNumber}\n${ccNumber}\n${expirationDate}`;
     console.log(person);
   }
+  return person;
 };
 
 const listOfOldCreditcards = () => {
   clearList();
-  const list = document.querySelector('#content');
-  list.innerText = `${dd}-${mm}-${yyyy}`;
+  person();
+
+  for (let i = 0; i < randomPersonData.length; i++) {
+    const fullName = `${randomPersonData[i].name} ${randomPersonData[i].surname}`;
+    const phoneNumber = `Phone: ${randomPersonData[i].phone}`;
+    const ccNumber = `Card: ${randomPersonData[i].credit_card.number}`;
+    const expirationDate = `Expires: ${randomPersonData[i].credit_card.expiration}`;
+    const person = `${fullName}\n${phoneNumber}\n${ccNumber}\n${expirationDate}`;
+    console.log(person);
+
+    const list = document.querySelector('#content');
+    const listItem = document.createElement('li');
+    const name = document.createElement('span');
+    name.innerHTML = fullName;
+    const phone = document.createElement('span');
+    phone.innerHTML = phoneNumber;
+    const card = document.createElement('span');
+    card.innerHTML = ccNumber;
+    const expires = document.createElement('span');
+    expires.innerHTML = expirationDate;
+
+    list.appendChild(listItem);
+    listItem.appendChild(name);
+    listItem.appendChild(phone);
+    listItem.appendChild(card);
+    listItem.appendChild(expires);
+  }
 };
 document
   .querySelector('#ouweCreditcards')
