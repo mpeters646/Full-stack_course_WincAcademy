c42e82c8bc5a2f60dc3db83cc77242c56d6fe61b
Marco Peters <m.peters646@gmail.com>
2021-05-27 14:35:55 +0200

    modified style.css & index.html & api-fun.js

---
+++ Level_24_Opdracht-2-API-Fun-Time/js/api-fun.js
@@ -0,0 +1,35 @@
+// Random Dad joke
+const getDadJoke = async () => {
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const json = await response.json();
+  console.log(`Dad Joke: ${json.joke}`);
+  console.log(json);
+};
+
+// Random Tronald Dump tweet
+const getTronaldDumpTweet = async () => {
+  const response = await fetch('http://api.tronalddump.io/random/quote', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const json = await response.json();
+  console.log(`Tronald Dump Tweet: ${json.value}`);
+};
+
+// PokeAPI
+const getPokemon = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+  const json = await response.json();
+  console.log(json);
+};
+// getPokemon();
+
+document.querySelector('#tweet').addEventListener('click', getTronaldDumpTweet);
+document.querySelector('#joke').addEventListener('click', getDadJoke);
