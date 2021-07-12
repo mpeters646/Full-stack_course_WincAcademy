b8bed9ee75f7a067a64689481f4a9b0612a985cb
Marco Peters <m.peters646@gmail.com>
2021-07-12 18:20:27 +0200

    modified functions.test.js

--- Level_27_Opdracht-1/functions.test.js
+++ Level_27_Opdracht-1/functions.test.js
@@ -15,7 +15,9 @@ test('checkValue Should be falsy when argument is undefined', () => {
 });
 
 test('User should be Brad Traversy object', () => {
-  // expect(functions.createUser().user).toBe('Brad Traversy');
+  expect(
+    functions.createUser().firstName + ' ' + functions.createUser().lastName
+  ).toBe('Brad Traversy');
 });
 
 // Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
@@ -26,15 +28,18 @@ test('Should be under or equal to 1600', () => {
   const load1 = 800;
   const load2 = 800;
   // expect(load1 + load2).toBe.....
+  expect(load1 + load2).toBe(1600);
 });
 
 // Regex
 test('There is no I in team', () => {
   //  expect('team').not.to..........;
+  expect('team').not.toMatch(/I/);
 });
 
 // Arrays
 test('Admin should be in usernames', () => {
   usernames = ['john', 'karen', 'admin'];
   // expect(usernames).to......
+  expect(usernames).toContain('admin');
 });
