def2f366fe6a85153327105c2f29197ac061fd43
Marco Peters <m.peters646@gmail.com>
2021-07-12 14:53:23 +0200

    modified functions.js & functions.test.js

--- Level_27_Opdracht-1/functions.js
+++ Level_27_Opdracht-1/functions.js
@@ -1,5 +1,17 @@
 const functions = {
-  add: (num1, num2) => num1 + num2,
+  isNull: function () {
+    return null;
+  },
+  checkValue: function (x) {
+    return x;
+  },
+  createUser: function () {
+    const user = {
+      firstName: 'Brad',
+    };
+    user['lastName'] = 'Traversy';
+    return user;
+  },
 };
 
 module.exports = functions;
--- Level_27_Opdracht-1/functions.test.js
+++ Level_27_Opdracht-1/functions.test.js
@@ -1,6 +1,40 @@
-const { TestWatcher } = require('jest');
-const functions = require('./functions');
+const functions = require('./functions.js');
 
-test('adds 2 + 2 to equal 4', () => {
-  expect(functions.add(2, 2)).tobe(4);
+test('Should be null', () => {
+  // Begin met expect()
+  // Binnen de eerste haken zet je hetgeen je wilt testen. Dat zal een functie zijn.
+  // Daarna volgt je .toBeSomethingSomething functie,
+  // zoek de geschikte functie in de documentatie.
+  // In dit geval willen we checken of de functie "isNull()" ook daadwerkelijk
+  // Null returned. Dus "toBeNull()" makes the most sense.
+  expect(functions.isNull()).toBeNull();
+});
+
+test('checkValue Should be falsy when argument is undefined', () => {
+  expect(functions.checkValue()).toBeUndefined();
+});
+
+test('User should be Brad Traversy object', () => {
+  // expect(functions.createUser().user).toBe('Brad Traversy');
+});
+
+// Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
+// nu tests maken die werken zonder input van buitenaf:
+
+// Less than or greater than
+test('Should be under or equal to 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  // expect(load1 + load2).toBe.....
+});
+
+// Regex
+test('There is no I in team', () => {
+  //  expect('team').not.to..........;
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  // expect(usernames).to......
 });
