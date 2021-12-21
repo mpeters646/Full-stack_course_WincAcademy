ac4ebef0ecb6f83909e6f2253a1cc0c9e3172df5
Marco Peters <m.peters646@gmail.com>
2021-12-21 17:38:23 +0100

    modified cat-breeds.js and script.js translate from dutch to english

--- Level_13_Opdracht-3/script.js
+++ Level_13_Opdracht-3/script.js
@@ -1,117 +1,56 @@
+const { catBreeds } = require("./cat-breeds.js");
+
 /* Deel 1: Objecten */
 
-// const person = {
-//   name: 'Marco',
-//   age: 50,
-//   evaluations: [7, 10, 9],
-// };
+const person = {
+  name: "Marco",
+  age: 50,
+  evaluations: [7, 10, 9],
+};
+console.log(person);
+console.log(" ");
+console.log("Dot notation");
+console.log(`Persons name: ${person.name}`);
+console.log(`Persons age: ${person.age}`);
+console.log(`Persons evaluations: ${person.evaluations}`);
 
-// console.log(person);
-// console.log(person.name);
-// console.log(person.age);
-
-// console.log(person['name']);
-// console.log(person['age']);
-// console.log(person['evaluations']); // Waarde is [7, 10, 9]
+console.log("");
+console.log("Bracket notation");
+console.log(`Persons name: ${person["name"]}`);
+console.log(`Persons age: ${person["age"]}`);
+console.log(`Persons evaluations: ${person["evaluations"]}`); // Waarde is [7, 10, 9]
+console.log("");
 
 /* Deel 2: Arrays */
 
-// let myArray = ['groen', 'rood', 'blauw'];
+let myArray = ["green", "red", "blue"];
 
-// console.log(myArray);
-// console.log(myArray.length);
-// console.log(myArray[0]);
-// console.log(myArray[myArray.length - 1]);
+console.log(myArray);
+console.log(myArray.length);
+console.log(myArray[0]);
+console.log(myArray[myArray.length - 1]);
 
-// console.log(myArray.push('geel'));
-// console.log(myArray.push(5));
-// console.log(myArray.push({ greeting: 'hi ik ben een object' }));
-// console.log(myArray);
-// console.log(myArray[myArray.length - 1]);
+console.log(myArray.push("yellow"));
+console.log(myArray.push(5));
+console.log(myArray.push({ greeting: "hi I'm a object" }));
+console.log(myArray);
+console.log(myArray[myArray.length - 1]);
+console.log("");
 
 /* Deel 3: Real-life */
 
-const catBreeds = [
-  {
-    name: 'Abyssinian',
-    description:
-      'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.',
-    dog_friendly: 4,
-    energy_level: 5,
-    life_span: '14 - 15',
-    origin: 'Egypt',
-    temperament: [
-      'Active',
-      'Energetic',
-      'Independent',
-      'Intelligent',
-      'Gentle',
-    ],
-    wikipedia_url: 'https://en.wikipedia.org/wiki/Abyssinian_(cat)',
-    food: {
-      favourite_food: 'fish',
-      medium_liked_food: 'dried fruits',
-      disliked_food: 'walnuts',
-    },
-  },
-  {
-    name: 'Aegean',
-    description:
-      'Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.',
-    dog_friendly: 4,
-    energy_level: 53,
-    life_span: '9- 12',
-    origin: 'Greece',
-    temperament: ['Affectionate', 'Social', 'Intelligent', 'Playful', 'Active'],
-    wikipedia_url: 'https://en.wikipedia.org/wiki/Aegean_cat',
-    food: {
-      favourite_food: 'tuna',
-      medium_liked_food: 'canned food',
-      disliked_food: 'all fruits',
-    },
-  },
-  {
-    name: 'American Bobtail',
-    description:
-      'American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.',
-    dog_friendly: 5,
-    energy_level: 3,
-    life_span: '11 - 15',
-    origin: 'United States',
-    temperament: [
-      'Intelligent',
-      'Interactive',
-      'Lively',
-      'Playful',
-      'Sensitive',
-    ],
-    wikipedia_url: 'https://en.wikipedia.org/wiki/American_Bobtail',
-    food: {
-      favourite_food: 'meaty things',
-      medium_liked_food: 'tuna',
-      disliked_food: 'canned food',
-    },
-  },
-];
+console.log(`Name last cat breed: ${catBreeds[catBreeds.length - 1].name}`);
+
+console.log(`Energy level of the first cat: ${catBreeds[0].energy_level}`);
 
 console.log(
-  'Naam van het laatste kattenras:',
-  catBreeds[catBreeds.length - 1].name
-);
-
-console.log('Energy levels van de eerste kat:', catBreeds[0].energy_level);
-
-console.log(
-  'Het eerste temperament van de tweede kat:',
-  catBreeds[1].temperament[0]
+  `First temperament of the second cat: ${catBreeds[1].temperament[0]}`
 );
 
 console.log(
-  'Het laatste temperament van de derde kat:',
-  catBreeds[2].temperament[4]
+  `Last temperament of the third cat: ${catBreeds[2].temperament[4]}`
 );
 
 console.log(
-  'Het favoriete voedsel van de derde kat:',
-  catBreeds[2].food.favourite_food
+  `Favorite food of the third cat: ${catBreeds[2].food.favourite_food}`
 );
