5190c0ce8e777cea0c0f96393be5eb06fb223f18
Marco Peters <m.peters646@gmail.com>
2021-12-23 12:30:05 +0100

    modified script.js | added extra lines for more readability

--- Level_14_Opdracht-1/script.js
+++ Level_14_Opdracht-1/script.js
@@ -1,22 +1,24 @@
 const makingPizza = function () {
-  console.log('take a pizza bottom');
-  console.log('cover the pizza base with tomato sauce');
-  console.log('sprinkel the pizza with cheese');
-  console.log('then put the salami on the pizza');
-  console.log('put the bell pepper, onions and olives on the pizza');
-  console.log('bake the pizza for 20 minutes in a oven');
-  console.log('enjoy your meal');
+  console.log("take a pizza bottom");
+  console.log("cover the pizza base with tomato sauce");
+  console.log("sprinkel the pizza with cheese");
+  console.log("then put the salami on the pizza");
+  console.log("put the bell pepper, onions and olives on the pizza");
+  console.log("bake the pizza for 20 minutes in a oven");
+  console.log("enjoy your meal");
+  console.log(" ***");
 };
 
 const makingSushi = function () {
-  console.log('boil water');
-  console.log('cook the rice');
-  console.log('let the rice cool down');
-  console.log('put a seaweed sheet on the sushi mat');
-  console.log('apply a layer of rice all over the sheet');
-  console.log('place the salmon and avocado in the middle of the rice');
-  console.log('roll the seaweed sheet from the bottom');
-  console.log('cut the roll into six pieces');
+  console.log("boil water");
+  console.log("cook the rice");
+  console.log("let the rice cool down");
+  console.log("put a seaweed sheet on the sushi mat");
+  console.log("apply a layer of rice all over the sheet");
+  console.log("place the salmon and avocado in the middle of the rice");
+  console.log("roll the seaweed sheet from the bottom");
+  console.log("cut the roll into six pieces");
+  console.log("***");
 };
 
 makingSushi();
