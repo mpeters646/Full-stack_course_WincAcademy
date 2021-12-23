f89d7a91a5267126e1bee7f9c7704756be718967
Marco Peters <m.peters646@gmail.com>
2021-12-23 15:58:44 +0100

    modified script.js

--- Level_14_Opdracht-2/script.js
+++ Level_14_Opdracht-2/script.js
@@ -1,13 +1,14 @@
 const paintingWalls = function (wallSide, color) {
-  console.log('The ' + wallSide + ' wall has been painted ' + color);
+  console.log("The " + wallSide + " wall has been painted " + color);
+  // console.log(`The ${wallSide} wall has been painted ${color}`);
 };
 
-paintingWalls('left', 'green');
-paintingWalls('right', 'yellow');
-paintingWalls('back', 'pink');
-paintingWalls('north', 'blue');
-paintingWalls('south', 'white');
-paintingWalls('east', 'black');
+paintingWalls("left", "green");
+paintingWalls("right", "yellow");
+paintingWalls("back", "pink");
+paintingWalls("north", "blue");
+paintingWalls("south", "white");
+paintingWalls("east", "black");
 
 // 6. the order of the arguments does matter when calling the function
 // 7. when you switch, your wall and color switching also
