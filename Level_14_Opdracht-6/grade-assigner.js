47f9ecc861975f35c2311eaec65bbaac2fdde2ce
Marco Peters <m.peters646@gmail.com>
2022-01-03 12:29:33 +0100

    modified grade-assigner.js translated to english

--- Level_14_Opdracht-6/grade-assigner.js
+++ Level_14_Opdracht-6/grade-assigner.js
@@ -1,18 +1,18 @@
 function assignGrade(score) {
   if (score > 90) {
-    return 'A';
+    return "A";
   } else if (score > 80) {
-    return 'B';
+    return "B";
   } else if (score > 70) {
-    return 'C';
+    return "C";
   } else if (score > 65) {
-    return 'D';
+    return "D";
   } else {
-    return 'F';
+    return "E";
   }
 }
 
 for (let i = 60; i <= 100; i++) {
   assignGrade((score = i));
-  console.log(`Voor ${i} punten, krijg je een ${assignGrade(score)}`);
+  console.log(`For scoring ${i} points, you get an ${assignGrade(score)}`);
 }
